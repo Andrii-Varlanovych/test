71de725002f21581a4350cc15ab1aa114bb244d4
Andrii Varlanovych <andrew_exp@ukr.net>
2023-01-11 12:26:31 +0200

    Hello was modificated

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         //hello
         //one more hello
+        //one more hello
         System.out.println("Hello world. Was typed on PC branch main. Was typed on Git branch main.");
     }
 }
