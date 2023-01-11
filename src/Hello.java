f9ab60c11367c068d84b25c898d49e2c39abfddf
Andrii-Varlanovych <116521427+Andrii-Varlanovych@users.noreply.github.com>
2023-01-11 11:35:53 +0200

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world. Was typed on PC branch main");
+        System.out.println("Hello world. Was typed on PC branch main. Was typed on Git branch main.");
     }
 }
